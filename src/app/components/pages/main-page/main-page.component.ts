072e1c83d8332f740bfba405b3c4a2cc32b9da56
StellarCrow <dariadar12@gmail.com>
2020-05-05 12:08:37 +0300

    add styles, material dialog

--- src/app/components/pages/main-page/main-page.component.ts
+++ src/app/components/pages/main-page/main-page.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddTodoComponent } from '../../add-todo/add-todo.component';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+  styleUrls: ['./main-page.component.scss'],
 })
 export class MainPageComponent {
+  constructor(public dialog: MatDialog) {}
 
+  public openDialog() {
+    const dialogRef = this.dialog.open(AddTodoComponent);
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+    });
+  }
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,8 +4,22 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialogModule } from '@angular/material/dialog';
 
-const components = [MatCheckboxModule, MatButtonModule, MatInputModule, MatDatepickerModule, MatNativeDateModule];
+const components = [
+  MatCheckboxModule,
+  MatButtonModule,
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatIconModule,
+  BrowserAnimationsModule,
+  MatTooltipModule,
+  MatDialogModule,
+];
 
 @NgModule({
   declarations: [],
